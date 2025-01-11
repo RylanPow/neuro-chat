ed6f8819649cc995f76781c533c2051422d12645
Rylan Pow <powrylan@gmail.com>
2025-01-11 05:37:22 -0800

    added public folder and assets

---
+++ src/layouts/rootLayout/RootLayout.jsx
@@ -0,0 +1,14 @@
+import './rootLayout.css'
+
+const RootLayout = () => {
+    return (
+        <div className="rootLayout">
+            <header>
+                <Link to = "/"> //TODO: download and add logo
+                </Link>
+            </header>
+        </div>
+    )
+}
+
+export default RootLayout
