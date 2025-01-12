f42acbb4d590b10f2b0135103b7361eafa7ec82d
Rylan Pow <powrylan@gmail.com>
2025-01-12 00:46:32 -0800

    fixed root layout, added dashboard layout

--- src/layouts/rootLayout/RootLayout.jsx
+++ src/layouts/rootLayout/RootLayout.jsx
@@ -1,14 +1,20 @@
+import { Link, Outlet } from "react-router-dom";
 import './rootLayout.css'
 
 const RootLayout = () => {
     return (
         <div className="rootLayout">
             <header>
-                <Link to = "/"> //TODO: download and add logo
+                <Link to = "/">
+                    <img src="/logo.png" alt="" />
+                    <span>NeuroChat</span>
                 </Link>
             </header>
+        <main>
+            <Outlet/>
+        </main>
         </div>
-    )
-}
+    );
+};
 
-export default RootLayout
+export default RootLayout;
--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { RouterProvider } from 'react-router-dom';
-import { createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Homepage from './routes/homepage/Homepage';
 import DashboardPage from './routes/dashboardPage/DashboardPage';
 import ChatPage from './routes/chatPage/ChatPage';  
+import RootLayout from './layouts/rootLayout/RootLayout';
+import DashboardLayout from './layouts/dashboardLayout/DashboardLayout';
 
 const router = createBrowserRouter([
     {
-        path: "/",
-        element: <Homepage/>,
-    },
-    {
-        path: "/dashboard",
-
+        element: <RootLayout/>,
         children: [
-            {path: "/dashboard", element: <DashboardPage />},
-            {path: "/dashboard/chats/:id", element: <ChatPage />},
+        {
+          path: "/",
+          element: <Homepage />
+        },
+        {
+          element: <DashboardLayout/>,
+          children: [
+              {
+                path: "/dashboard",
+                element: <DashboardPage/>,
+              },
+              {
+                path: "/dashboard/chats/:id",
+                element: <ChatPage />,
+              },
+            ],
+          },
         ],
     },
 ]);
