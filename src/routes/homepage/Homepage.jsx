4193cbeddcfc802988d911a3d638a5dc20c6259c
Rylan Pow <powrylan@gmail.com>
2025-01-13 02:43:14 -0800

    bot animation, index.html name change

--- src/routes/homepage/Homepage.jsx
+++ src/routes/homepage/Homepage.jsx
@@ -8,11 +8,18 @@ const Homepage = () => {
         <h1>NeuroChat</h1>
         <h2>Supercharge productivity with the latest in LLM technology</h2>
         <h3>
-        Wumpus Thinking Dancing Eating Sleeping Blurple
+            Wumpus Thinking Dancing Eating Sleeping Blurple
         </h3>
         <Link to="/dashboard">Get Started</Link>
     </div>
-        <div className="right"></div>
+        <div className="right">
+            <div className = 'imgContainer'>
+                <div className="bgContainer">
+                    <div className='bg'></div>
+                </div>
+                <img src="bot.png" alt="" className ='bot'/>
+            </div>
+        </div>
     </div>
     )
 }
