69bc60041963b41813bf3c34d4afbe4a9e495aeb
Rylan Pow <powrylan@gmail.com>
2025-01-12 23:25:22 -0800

    made homepage pretty

--- src/routes/homepage/Homepage.jsx
+++ src/routes/homepage/Homepage.jsx
@@ -2,9 +2,17 @@ import './homepage.css'
 import { Link } from 'react-router-dom'
 const Homepage = () => {
     return (
-       <div className="homepage">
-        <Link to ="/dashboard">Dashboard</Link>
-        </div>
+    <div className="homepage">
+        <div className="left">
+        <h1>NeuroChat</h1>
+        <h2>Supercharge productivity with the latest in LLM technology</h2>
+        <h3>
+        Wumpus Thinking Dancing Eating Sleeping Blurple
+        </h3>
+        <Link to="/dashboard">Get Started</Link>
+    </div>
+        <div className="right"></div>
+    </div>
     )
 }
 
