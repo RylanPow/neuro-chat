b107a7d18300c16a6fd9179d4e8cdd59eede252b
Rylan Pow <powrylan@gmail.com>
2025-01-14 06:10:18 -0800

    fleshed out dashboard client side

--- src/components/chatList/ChatList.jsx
+++ src/components/chatList/ChatList.jsx
@@ -4,11 +4,12 @@ import { Link } from 'react-router-dom'
 const  ChatList = () => {
     return (
         <div className = 'chatList'>
-            <span className = 'title'>DASHBOARD</span>
+        <span className = 'title'>DASHBOARD</span>
         <Link to = 'dashboard'>Create a new Chat</Link>
         <Link to = '/'>Explore NeuroChat</Link>
         <Link to = '/'>Contact</Link>
         <hr/>
+        <span className ='title'>RECENT CHATS</span>
         <div className='list'>
             <Link to='/'>My Chat Title</Link>
             <Link to='/'>My Chat Title</Link>
@@ -20,15 +21,15 @@ const  ChatList = () => {
         </div>
         <hr/>
         <div className = 'upgrade'>
-            <img src = '/logo.png' alt =''>
+            <img src = '/logo.png' alt =''/>
                 <div className = 'texts'>
                     <span>Upgrade to NeuroChat Pro(not actually a thing)</span>
                     <span>Unlimited access to all features!</span>
                 </div>
-            </img>
-        </div>
-        
 
+         </div>
         </div>
     )
 }
+
+export default ChatList;
