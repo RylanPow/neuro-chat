44c1466c903b1b31e0bf608e218f802e8bc96ad3
Rylan Pow <powrylan@gmail.com>
2025-01-20 20:02:53 -0800

    chat from dashboard now redirects

--- client/src/routes/chatPage/ChatPage.jsx
+++ client/src/routes/chatPage/ChatPage.jsx
@@ -29,9 +29,24 @@ const ChatPage = () => {
                     : error 
                     ? "Something went wrong!" 
                     : data?.history?.map((message, i) => (
-                        <div className = "message user" key = {i}>
+                        <>
+                        {message.img && (
+                            <IKImage urlEndpoint = {import.meta.env.VITE_IMAGE_KIT_ENDPOINT}
+                            path = {message.img}
+                            height = "300"
+                            width = "400"
+                            transformation = {[{height: 300, width: 400}]}
+                            loading = "lazy"
+                            lqip = {{active: true, quality: 20}}
+
+                            />
+                        )}
+                        <div className = {
+                            message.role === "user" ? "message user" : "message"
+                        } key = {i}>
                             <Markdown>{message.parts[0].text}</Markdown>
                         </div>
+                        </>
                     )
                 )}
                     <NewPrompt/>
