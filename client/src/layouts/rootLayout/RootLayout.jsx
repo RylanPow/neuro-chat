fc4f35c5c2835d733e6369a47421d7dac707be91
Rylan Pow <powrylan@gmail.com>
2025-01-23 16:45:44 -0800

    updated modules, changed rootlayout signin

--- client/src/layouts/rootLayout/RootLayout.jsx
+++ client/src/layouts/rootLayout/RootLayout.jsx
@@ -1,46 +1,38 @@
 import { Link, Outlet } from "react-router-dom";
-import './rootLayout.css'
-import { ClerkProvider, SignedOut, SignedIn, SignInButton, UserButton } from "@clerk/clerk-react";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-
-
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY //how to import .env variables without exposure
+import "./rootLayout.css";
+import { ClerkProvider, SignedIn, UserButton } from "@clerk/clerk-react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key")
+  throw new Error("Missing Publishable Key");
 }
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 const RootLayout = () => {
-    return (
-        <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/"
-        debug ={true}
-        >
-            <QueryClientProvider client = {queryClient}>
+  return (
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <QueryClientProvider client={queryClient}>
         <div className="rootLayout">
-            <header>
-                <Link to = "/" className="logo">
-                    <img src="/logo.png" alt="" />
-                    <span>NeuroChat</span>
-                </Link>
-
-                <div className = "user">
-                    <SignedOut>
-                        <SignInButton/>
-                    </SignedOut>
-                    <SignedIn>
-                        <UserButton />
-                    </SignedIn>
-                </div>
-                </header>
-            <main>
-                <Outlet/>
-            </main>
+          <header>
+            <Link to="/" className="logo">
+              <img src="/logo.png" alt="" />
+              <span>LAMA AI</span>
+            </Link>
+            <div className="user">
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
             </div>
-            </QueryClientProvider>
-        </ClerkProvider>
-    );
+          </header>
+          <main>
+            <Outlet />
+          </main>
+        </div>
+      </QueryClientProvider>
+    </ClerkProvider>
+  );
 };
 
 export default RootLayout;
